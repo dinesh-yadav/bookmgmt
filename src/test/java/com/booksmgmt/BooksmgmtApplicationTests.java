df3690cb6bc5febbed1a5a0029c86dfd14dedcac
dinesh-yadav <yadav.din@gmail.com>
2025-11-18 16:27:44 +0530

    initial commit

---
+++ src/test/java/com/booksmgmt/BooksmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksmgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
