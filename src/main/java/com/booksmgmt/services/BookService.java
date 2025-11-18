e71bf0dd6f436aa430dc6280e33a094ef9393e60
dinesh-yadav <yadav.din@gmail.com>
2025-11-18 17:04:44 +0530

    add BookService interface

--- src/main/java/com/booksmgmt/services/BookService.java
+++ src/main/java/com/booksmgmt/services/BookService.java
@@ -1,4 +1,14 @@
 package com.booksmgmt.services;
 
+import com.booksmgmt.entities.Book;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface BookService {
+    public Mono<Book> create(Book book);
+    public Flux<Book> getAll();
+    public Mono<Book> get(int bookId);
+    public Mono<Book> update(int bookId);
+    public Mono<Void> delete(int bookId);
+    public Flux<Book> search(String query);
 }
